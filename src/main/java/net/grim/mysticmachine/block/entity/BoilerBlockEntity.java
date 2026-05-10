ea3d4eba98237dc905fec19d9205109136706ca4
JacobVespa <vespajacob@gmail.com>
2026-05-10 12:21:17 -0400

    Boiler Fuel Consumption and Fluid Production
    Boiler can accept coal as a fuel source
    Boiler generates steam when adjacent to water and provided coal
    
    GUI is next to display and input items

--- src/main/java/net/grim/mysticmachine/block/entity/BoilerBlockEntity.java
+++ src/main/java/net/grim/mysticmachine/block/entity/BoilerBlockEntity.java
@@ -5,20 +5,33 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluids;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.capability.IFluidHandler;
+import net.neoforged.neoforge.fluids.capability.templates.FluidTank;
 import net.neoforged.neoforge.items.ItemStackHandler;
+import net.minecraft.tags.ItemTags;
 
 public class BoilerBlockEntity extends BlockEntity {
 
-    // Add item/fluid storage fields here later, e.g.:
-    private final ItemStackHandler itemHandler = new ItemStackHandler(1);
-    // private final FluidTank waterTank = new FluidTank(8000);
-    // private final FluidTank steamTank = new FluidTank(8000);
+    // Fluid containers
+    //private final FluidTank waterTank = new FluidTank(8000);
+    private final FluidTank steamTank = new FluidTank(8000);
 
-    private int progress = 0;
-    private int maxProgress = 100;
+    private int litTime = 0;
+    private int steamTickRate = 2;
+
+    private final ItemStackHandler itemHandler = new ItemStackHandler(1) {
+        @Override
+        public boolean isItemValid(int slot, ItemStack stack)
+        {
+            return stack.is(ItemTags.COALS);
+        }
+    };
 
     public BoilerBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.BOILER_BE.get(), pos, state);
@@ -28,7 +41,30 @@ public class BoilerBlockEntity extends BlockEntity {
     public void tick(Level level, BlockPos pos, BlockState state) {
         if (level.isClientSide()) return;
 
+        boolean saveFlag = false;
+        
+        if (isLit()) {
+            produceSteam();
+            --litTime;
+        }
 
+        if (!isLit() && canProduceSteam(level,pos)) {
+
+            saveFlag = true;
+
+            ItemStack stack = itemHandler.getStackInSlot(0);
+
+            litTime = stack.getBurnTime(null);
+
+            if (isLit()) {
+                stack.shrink(1);
+            }
+        }
+
+        if (saveFlag)
+        {
+            setChanged();
+        }
 
     }
 
@@ -36,7 +72,9 @@ public class BoilerBlockEntity extends BlockEntity {
     @Override
     protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
         super.saveAdditional(tag, registries);
-        tag.putInt("Progress", progress);
+
+        tag.putInt("LitTime", litTime);
+        steamTank.writeToNBT(registries,tag);
 
         tag.put("Inventory", itemHandler.serializeNBT(registries));
     }
@@ -45,7 +83,9 @@ public class BoilerBlockEntity extends BlockEntity {
     @Override
     protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
         super.loadAdditional(tag, registries);
-        progress = tag.getInt("Progress");
+
+        litTime = tag.getInt("LitTime");
+        steamTank.readFromNBT(registries,tag);
 
         itemHandler.deserializeNBT(registries, tag.getCompound("Inventory"));
     }
@@ -59,5 +99,24 @@ public class BoilerBlockEntity extends BlockEntity {
         }
 
         return false;
+    }
+
+    private boolean canProduceSteam(Level level, BlockPos pos)
+    {
+        if (!itemHandler.getStackInSlot(0).isEmpty() && itemHandler.isItemValid(0,itemHandler.getStackInSlot(0)) && steamTank.getFluidAmount() < steamTank.getCapacity()) { //Checks if coal is present and steam reserve has space
+            return isAdjacentToWater(level, pos); //Checks if boiler is next to water block
+        }
+
+        return false;
+    }
+
+    private void produceSteam()
+    {
+        steamTank.fill(new FluidStack(Fluids.WATER, steamTickRate), IFluidHandler.FluidAction.EXECUTE); //TODO Add steam later to replace water
+    }
+
+    private boolean isLit()
+    {
+        return this.litTime > 0;
     }
 }
